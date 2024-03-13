9c48a5498f4bab8662b03880c7d2abe8b629f8d8
Hari Om Singh <Hariomsingh4274@gmail.com>
2024-03-14 00:35:46 +0530

    happu

--- Components/Dashboard.jsx
+++ Components/Dashboard.jsx
@@ -84,21 +84,7 @@ else{
 
     
       <section class="home-section">
-      <nav>
-        <div class="sidebar-button">
-          <i class="bx bx-menu sidebarBtn"></i>
-          <span class="dashboard">Dashboard</span>
-        </div>
-        {/* <!-- <div class="search-box">
-          <input type="text" placeholder="Search..." />
-          <i class="bx bx-search"></i>
-        </div> --!> */}
-        <div class="profile-details">
-          {/* <img src="images/profile.jpg" alt="" /> */}
-          <span class="admin_name"> Welcome {Name}</span>
-          {/* <i class="bx bx-chevron-down"></i> */}
-        </div>
-      </nav>
+    
 
       <div class="home-content">
         <br />
