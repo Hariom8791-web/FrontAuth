c9b57ee359f2288e5c913bdc55cf5ceab22ffcfd
Hari Om Singh <Hariomsingh4274@gmail.com>
2024-03-16 10:17:11 +0530

    SS

--- Components/Services/ChetakMail.jsx
+++ Components/Services/ChetakMail.jsx
@@ -63,7 +63,7 @@ function ChetakMail() {
     setdisplaytext("We are Sending Your Emails >> Do not Reload page")
     console.log(htmlFile)
      // axios.post(config.API_URL+'/auth/ChetakMail', { emails,textmsg,subject,htmlFile,name})
-     axios.post('https://821c-103-211-191-74.ngrok-free.app/Service/Chetak', { emails,textmsg,subject,htmlFile,name})
+     axios.post('https://6c78-2a09-bac1-36e0-1d0-00-47-49e.ngrok-free.app/Service/Chetak', { emails,textmsg,subject,htmlFile,name})
      .then(res =>{
         console.log(res.data.message)
         if(res.data.status){
